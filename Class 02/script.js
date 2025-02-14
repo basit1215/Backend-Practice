a63a5b077017c09f36e1520f1e9c42e2d749a518
malik1243 <154369335+basit1215@users.noreply.github.com>
2025-02-14 23:46:53 +0500

    updated

---
+++ Class 02/script.js
@@ -0,0 +1,33 @@
+const fs = require('node:fs');
+
+// fs.writeFile("hey.txt", "hey hello kaise ho", function(err) {
+//     if (err) console.error(err);
+//     else  console.log("File saved!");
+// })
+
+// fs.appendFile("hey.txt", " Alhamdullilah!", function(err) {
+//     if (err) console.error(err);
+//     else  console.log("File updated!");
+// })
+
+// fs.rename("hey.txt", "hello.txt", function(err) {
+//     if (err) console.error(err);
+//     else  console.log("File Name Changed!");
+// })
+
+// fs.copyFile("hello.txt", "./copy/chacha.txt", function(err){
+//     if (err) console.error(err);
+//     else  console.log("File copied!");
+// })
+
+// Check the error message properly
+
+// fs.copyFile("hello.txt", "./copy2/chacha.txt", function(err){
+//     if (err) console.error(err.message);
+//     else  console.log("File copied!");
+// })
+
+fs.unlink("hello.txt", function(err){
+    if (err) console.error(err);
+    else  console.log("File deleted!");
+})
